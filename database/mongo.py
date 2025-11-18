c5f751458c6b585c292a743fe54616e93119976a
SAHXYZ <opsahalam97@gmail.com>
2025-11-18 19:16:39 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,33 +1,27 @@
 from pymongo import MongoClient
-from config import MONGO_URI
+from config import MONGO_URI, DB_NAME
 
 client = MongoClient(MONGO_URI)
-db = client["GameBot"]
+db = client[DB_NAME]
 users = db["users"]
 
-
-def get_user(user_id):
-    user_id = str(user_id)
-    user = users.find_one({"_id": user_id})
-
+def get_user(uid):
+    uid = str(uid)
+    user = users.find_one({"_id": uid})
     if not user:
         user = {
-            "_id": user_id,
-            "coins": 0,
-            "group_msgs": 0,
-            "cooldowns": {},
-            "profile": {},
-            "inventory": [],
-            "badges": []
+            "_id": uid,
+            "black_gold": 0,
+            "platinum": 0,
+            "gold": 0,
+            "silver": 0,
+            "bronze": 0,
+            "messages": 0,
+            "badges": [],
+            "inventory": []
         }
         users.insert_one(user)
-
     return user
 
-
-def update_user(user_id, data: dict):
-    users.update_one(
-        {"_id": str(user_id)},
-        {"$set": data},
-        upsert=True
-    )
+def update_user(uid, data: dict):
+    users.update_one({"_id": str(uid)}, {"$set": data})
