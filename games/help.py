1510098e6c6b5ea2c7fa2d6c4bdee0d07b7ac271
SAHXYZ <opsahalam97@gmail.com>
2025-11-20 09:02:26 +0530

    Update help.py

--- games/help.py
+++ games/help.py
@@ -1,34 +1,36 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.handlers import MessageHandler
+
+def _help(_, msg: Message):
+    text = (
+        "🎮 **GameBot Help Menu**\n\n"
+        "**General Commands**\n"
+        "/start - Show menu\n"
+        "/help - Show this help message\n"
+        "/profile - View your profile\n"
+        "/leaderboard - Top players\n\n"
+        "**Mining System**\n"
+        "/mine - Mine ores\n"
+        "/sell - Sell ores\n"
+        "/tools - View your tools\n"
+        "/equip <tool> - Equip a tool\n"
+        "/repair - Repair your tool\n\n"
+        "**Economy & Items**\n"
+        "/work - Earn bronze\n"
+        "/shop - Buy items\n"
+        "/buy <num> - Purchase an item\n"
+        "/inv - View your inventory\n\n"
+        "**Games**\n"
+        "/flip - Coin flip\n"
+        "/roll - Dice roll\n"
+        "/fight - Fight another user\n"
+        "/rob - Rob a user\n"
+        "/guess - Guessing game\n"
+    )
+
+    msg.reply(text)
 
 def init_help(bot: Client):
-
-    @bot.on_message(filters.command("help") & filters.private)
-    async def help_cmd(_, msg: Message):
-        text = (
-            "🎮 **GameBot Help Menu**\n\n"
-            "**General Commands**\n"
-            "/start - Show menu\n"
-            "/help - Show this help message\n"
-            "/profile - View your profile\n"
-            "/leaderboard - View top players\n\n"
-            "**Coin & Inventory**\n"
-            "/work - Earn bronze by working\n"
-            "/shop - Purchase special items\n"
-            "/buy <item_number> - Buy an item\n"
-            "/inv - View your items/ores\n\n"
-            "**Mining System**\n"
-            "/mine - Mine ores using your equipped pickaxe\n"
-            "/sell - Sell ores using inline buttons\n"
-            "/tools - View your tools\n"
-            "/equip <tool> - Equip a pickaxe\n"
-            "/repair - Repair your equipped tool\n\n"
-            "**Games**\n"
-            "/flip - Coin flip game\n"
-            "/roll - Random dice roll\n"
-            "/fight - Fight a user\n"
-            "/rob - Rob a player\n"
-            "/guess - Guess the word\n"
-        )
-
-        await msg.reply(text)
+    bot.add_handler(MessageHandler(_help), filters=filters.command("help") & filters.private)
+    print("[loaded] games.help")
